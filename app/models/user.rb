71f1d7c3f9f816eee814d79bb9b9880863f92027
d-otis <dan@dan-foley.com>
2021-05-29 22:17:55 -0400

    add User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  validates :email, :uniqueness => true, :presence => true
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
+  validates :is_admin, :presence => true
+
+
   def full_name
     "#{first_name} #{last_name}"
   end
