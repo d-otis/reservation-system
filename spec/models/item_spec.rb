7285c42af4e41ada81d68404eeae606145e0a0a7
d-otis <dan@dan-foley.com>
2021-05-30 16:12:37 -0400

    update Item spec

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 require 'item_examples'
+require 'reservation_examples'
+require 'user_examples'
+require 'brand_examples'
 
 RSpec.describe Item, type: :model do
   include_examples "item examples"
+  include_examples "reservation examples"
+  include_examples "user examples"
+  include_examples "brand examples"
+
+  let(:valid_item_attrs_with_brand) { valid_item_attrs.merge(brand: brand) }
 
   context "Attributes" do
     it "has a name attr" do
@@ -27,8 +35,6 @@ RSpec.describe Item, type: :model do
   end
 
   context "Validations" do
-    let(:brand) { Brand.new(name: "MOTU") }
-    let(:valid_item_attrs_with_brand) { valid_item_attrs.merge(brand: brand) }
     let(:item_missing_name) { valid_item_attrs_with_brand.except(:name) }
     let(:item_missing_description) { valid_item_attrs_with_brand.except(:description) }
     let(:item_missing_brand) { valid_item_attrs }
@@ -57,9 +63,33 @@ RSpec.describe Item, type: :model do
 
   context "Associations" do
     it "belongs to a Brand" do
-      brand = Brand.create(name: "MOTU")
       item = Item.create(valid_item_attrs.merge(brand_id: brand.id))
       expect(item.brand).to eq(brand)
+    end
+
+    it "belongs to a Reservation" do
+      reservation = test_user.reservations.create(valid_reservation_attrs)
+      item = Item.create(valid_item_attrs_with_brand)
+      item.reservations << reservation
+      expect(item.reservations.include?(reservation)).to be(true)
+      expect(item.reservations).to eq([reservation])
+    end
+
+    it "can belong to more than one Reservation" do
+      reservation = test_user.reservations.create(valid_reservation_attrs)
+      reservation_2 = test_user.reservations.create(valid_reservation_attrs)
+      item = Item.create(valid_item_attrs_with_brand)
+      item.reservations << reservation
+      item.reservations << reservation_2
+      expect(item.reservations.count).to eq(2)
+      expect(item.reservations).to eq([reservation, reservation_2])
+    end
+
+    it "the same Item can only be added to a Reservation once" do
+      reservation = test_user.reservations.create(valid_reservation_attrs)
+      item = Item.create(valid_item_attrs_with_brand)
+      reservation.items << item
+      expect { reservation.items << item }.to raise_error(ActiveRecord::RecordInvalid )
     end
   end
 end
