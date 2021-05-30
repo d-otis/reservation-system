c4b3579df413a666b5ab4b03b499ecb9a3939dc6
d-otis <dan@dan-foley.com>
2021-05-30 15:08:56 -0400

    add additional item to item_examples

--- spec/item_examples.rb
+++ spec/item_examples.rb
@@ -5,5 +5,11 @@ RSpec.shared_examples "item examples" do
     :serial_number => "xyz123"
   } end
 
+  let(:more_valid_item_attrs) do {
+    :name => "SM58 Microphone",
+    :description => "Nulla vitae elit libero, a pharetra augue.",
+    :serial_number => "123xyz"
+  } end
+
   let(:item_missing_name) { valid_item_attrs.except(:name) }
 end
