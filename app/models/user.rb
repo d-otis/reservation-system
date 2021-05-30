3df742bab46c9b0880e30f51a435b2e49c50fc76
d-otis <dan@dan-foley.com>
2021-05-30 13:41:45 -0400

    wire up association between User and Reservation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
+  has_many :reservations
+
   validates :email, :uniqueness => true, :presence => true
   validates :first_name, :presence => true
   validates :last_name, :presence => true
   validates :is_admin, :presence => true
-
 
   def full_name
     "#{first_name} #{last_name}"
