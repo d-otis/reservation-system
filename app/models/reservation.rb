435d0e9aae45bd7a81bac3165f65746b16a6a67b
d-otis <dan@dan-foley.com>
2021-05-30 15:10:40 -0400

    add validations for Reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,15 @@
 class Reservation < ApplicationRecord
   belongs_to :user
+
+  validates :start_time, :end_time, :presence => true
+  validate :end_after_start
+
+  private
+
+  def end_after_start
+    return if start_time.blank? || end_time.blank?
+    if start_time > end_time
+      errors.add(:end_time, "must be after start time")
+    end
+  end
 end
