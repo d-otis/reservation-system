799fa3d6ab0237923457a6d222726f2cff4d056f
d-otis <dan@dan-foley.com>
2021-05-30 15:10:16 -0400

    inital specs for Reservation

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,5 +1,84 @@
 require 'rails_helper'
+require 'reservation_examples'
+require 'user_examples'
+require 'item_examples'
 
 RSpec.describe Reservation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include_examples "user examples"
+  include_examples "reservation examples"
+  include_examples "item examples"
+
+  let(:reservation_attrs_with_user) { valid_reservation_attrs.merge(user: test_user) }
+  let(:brand) { Brand.create(name: "Shure") }
+
+  context "Attributes" do
+    it "has a start_time attribute" do
+      expect(Reservation.new).to respond_to(:start_time)
+    end
+
+    it "has a end_time attribute" do
+      expect(Reservation.new).to respond_to(:end_time)
+    end
+
+    it "has a note attribute" do
+      expect(Reservation.new).to respond_to(:note)
+    end
+
+    it "has a user_id attribute" do
+      expect(Reservation.new).to respond_to(:user_id)
+    end
+
+    it "has a user macro attribute" do
+      expect(Reservation.new).to respond_to(:user)
+    end
+
+    it "has an items macro attribute" do
+      expect(Reservation.new).to respond_to(:items)
+    end
+  end
+
+  context "Validations" do
+    it "is valid with valid attributes" do
+      reservation = Reservation.new(reservation_attrs_with_user)
+      expect(reservation).to be_valid
+    end
+
+    it "is invalid without a User" do
+      expect(Reservation.new(valid_reservation_attrs)).to be_invalid
+    end
+
+    it "is valid with missing note" do
+      expect(Reservation.new(reservation_attrs_with_user.except(:note))).to be_valid
+    end
+
+    it "is invalid with missing start_time" do
+      expect(Reservation.new(reservation_attrs_with_user.except(:start_time))).to be_invalid
+    end
+
+    it "is invalid with missing end_time" do
+      expect(Reservation.new(reservation_attrs_with_user.except(:end_time))).to be_invalid
+    end
+
+    it "is invalid with end_time previous to start_time" do
+      start_time = DateTime.now
+      end_time = start_time - 2
+      reservation = Reservation.new(user: test_user, start_time: start_time, end_time: end_time)
+      expect(reservation).to be_invalid
+    end
+  end
+
+  context "Associations" do
+    it "is owned by a User" do
+      reservation = Reservation.create(reservation_attrs_with_user)
+      expect(test_user.reservations.include?(reservation)).to be(true)
+    end
+    
+    it "has many Items" do
+      item = Item.create(valid_item_attrs.merge(:brand => brand))
+      reservation = Reservation.create(reservation_attrs_with_user)
+      reservation.items << item
+      expect(reservation.items.include?(item)).to be(true)
+      expect(reservation.items.count).to eq(1)
+    end
+  end
 end
