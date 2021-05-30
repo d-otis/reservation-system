48fce2814ca761061ee53ebb5ef0299f1daecbe4
d-otis <dan@dan-foley.com>
2021-05-29 22:30:06 -0400

    add user_examples

---
+++ spec/user_examples.rb
@@ -0,0 +1,12 @@
+RSpec.shared_examples "user examples" do
+  let(:valid_user_attrs) do 
+    {
+      :first_name => "Patsy",
+      :last_name => "Cline",
+      :email => "bluemoonkentucky@gmail.com",
+      :is_admin => true
+      # :password => "crazy!1234%^&",
+      # :password_confirmation => "crazy!1234%^&"
+    }
+  end
+end
