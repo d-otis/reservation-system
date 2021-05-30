6c4d00cd7cefd58c4a85aca28d12cedda9f38963
d-otis <dan@dan-foley.com>
2021-05-29 22:14:26 -0400

    generate Brand model, spec, migration

---
+++ spec/models/brand_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+require 'item_examples'
+
+RSpec.describe Brand, type: :model do
+  include_examples "item examples"
+
+  let(:valid_attrs) do {
+    :name => "MOTU"
+  } end
+
+  context "Attributes" do
+    it "has name attr"
+  end
+
+  context "Validations" do
+    it "is valid w/ a name"
+    it "is invalid w/o a name"
+  end
+
+  context "associations" do
+    it "has many Items" do
+      brand = Brand.create(valid_attrs)
+      item = Item.create(valid_item_attrs.merge(brand: brand))
+      expect(brand.items.count).to eq(1)
+      expect(brand.items.include?(item)).to be(true)
+    end
+  end
+end
