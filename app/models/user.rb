ba891cf50eabf5f6e1c43a1d2d5b4b5c70e1c6ae
d-otis <dan@dan-foley.com>
2021-05-31 19:28:44 -0400

    correct validation of boolean attr

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   validates :email, :uniqueness => true, :presence => true
   validates :first_name, :presence => true
   validates :last_name, :presence => true
-  validates :is_admin, :presence => true
+  validates_inclusion_of :is_admin, in: [true, false]
 
   def full_name
     "#{first_name} #{last_name}"
