c65e68664a1cdc0d10072f7197afbf3d0b2ea5eb
d-otis <dan@dan-foley.com>
2021-06-02 16:04:22 -0400

    add error-handling for AuthenticationController re: missing params

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,7 +1,16 @@
 class Api::V1::AuthenticationController < ApplicationController
+  rescue_from ActionController::ParameterMissing, with: :parameter_missing
+
   def create
-    puts params.inspect
+    puts params.require(:email).inspect
+    puts params.require(:password).inspect
 
     render json: { token: '123' }, status: :created
+  end
+
+  private
+
+  def parameter_missing(e)
+    render json: { error: e.message }, status: :unprocessable_entity
   end
 end
