545b8561fcd9dcad8cab5552555c9f1880a1e0cd
d-otis <dan@dan-foley.com>
2021-06-03 17:13:29 -0400

    move some error-handling to main ApplicationController

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::AuthenticationController < ApplicationController
   class AuthenticationError < StandardError; end
-  rescue_from ActionController::ParameterMissing, with: :parameter_missing
   rescue_from AuthenticationError, with: :handle_unauthenticated
 
   def create
@@ -13,10 +12,6 @@ class Api::V1::AuthenticationController < ApplicationController
 
   def user
     @user ||= User.find_by(email: params.require(:email))
-  end
-
-  def parameter_missing(e)
-    render json: { error: e.message }, status: :unprocessable_entity
   end
 
   def handle_unauthenticated
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Token
+  rescue_from ActiveRecord::RecordNotFound, with: :resource_not_found
+  rescue_from ActionController::ParameterMissing, with: :parameter_missing
 
   private
   
@@ -13,5 +15,13 @@ class ApplicationController < ActionController::API
 
   def check_admin_privileges
     render status: :unauthorized if !@user.is_admin?
+  end
+
+  def resource_not_found(e)
+    render json: { errors: [ e.message ] }, status: :not_found
+  end
+
+  def parameter_missing(e)
+    render json: { errors: [ e.message ] }, status: :unprocessable_entity
   end
 end
