698ffe2c1bcaa12609702c4cfa65f82aa66da157
d-otis <dan@dan-foley.com>
2021-06-03 13:03:27 -0400

    define ApplicationController private method authenticate_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token
+
+  private
+  
+  def authenticate_user
+    puts "inside authenticate"
+    token, _options = token_and_options(request)
+    user_id = AuthenticationTokenService.decode(token)
+    @user ||= User.find(user_id)
+  rescue ActiveRecord::RecordNotFound
+    render status: :unauthorized
+  end
+  
 end
