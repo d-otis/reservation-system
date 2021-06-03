bff39670f44e39a1e68c3031baa838e4bf6926cd
d-otis <dan@dan-foley.com>
2021-06-03 13:09:28 -0400

    add factory helper for generating reservation with items

--- spec/factories/reservations.rb
+++ spec/factories/reservations.rb
@@ -6,3 +6,9 @@ FactoryBot.define do
     user
   end
 end
+
+def reservation_with_items(item_count: 3)
+  FactoryBot.create(:reservation) do |reservation|
+    FactoryBot.create_list(:item, item_count, reservations: [reservation])
+  end
+end
