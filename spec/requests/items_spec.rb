f5bf568739a4a6c9dcd72127e2f6b43fc3871cca
d-otis <dan@dan-foley.com>
2021-06-03 16:13:37 -0400

    remove obsolete test

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -13,13 +13,6 @@ describe "Items API" do
       expect(response).to have_http_status(:success)
       expect(response_body['data'].size).to eq(1)
     end
-
-    it 'returns 401 Forbidden if user not authenticated' do
-      get '/api/v1/items', 
-        headers: { "Authorization" => "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxMjM0fQ.EH4nK1B1jv96X5-LRniQWg6OYcQzJG50L7YLY856ks4" }
-
-      expect(response).to have_http_status(:unauthorized)
-    end
   end
 
   context 'POST /items' do
