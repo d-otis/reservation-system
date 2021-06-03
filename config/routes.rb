2eb7e861d554f9a1190f95d176ec468a1bf75dfb
d-otis <dan@dan-foley.com>
2021-06-03 13:34:47 -0400

    add items routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :reservations, :except => [:show]
+      resources :items, :except => [:show]
       post 'authenticate', to: 'authentication#create'
     end
   end
