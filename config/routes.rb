c93b07c73e253e10f0da43c60a297585f941132a
d-otis <dan@dan-foley.com>
2021-06-08 19:30:07 -0400

    add create and index routes for BrandsController

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :reservations, :except => [:show]
       resources :items, :except => [:show]
+      resources :brands, :except => [:show]
       post 'authenticate', to: 'authentication#create'
     end
   end
